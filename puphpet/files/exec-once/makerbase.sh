fa993fb30449c616df338695cd917cc809cee05d
Gina Trapani <ginatrapani@gmail.com>
2015-02-19 13:05:46 -0500

    Auto-create Elasticsearch indices

--- puphpet/files/exec-once/makerbase.sh
+++ puphpet/files/exec-once/makerbase.sh
@@ -3,8 +3,8 @@ cp /var/www/puphpet/files/makerbase-setup/configs/config.inc.php /var/www/webapp
 
 # Make data dir and set perms
 mkdir /home/vagrant/data
-chown -R www-data /home/vagrant/data/
 
+chown -R www-data /home/vagrant/data/
 
 # Load sample data
 mysql -u makerbase -pnice2bnice -D makerbase_web < /var/www/sql/sample-data/sample-data.sql
@@ -13,15 +13,45 @@ mysql -u makerbase -pnice2bnice -D makerbase_web < /var/www/sql/sample-data/samp
 # Switch to home dir
 cd /usr/share/elasticsearch
 
-# Install ES to JDBC plugin
+# Install Elasticsearch to JDBC plugin
 ./bin/plugin --install jdbc --url http://xbib.org/repository/org/xbib/elasticsearch/plugin/elasticsearch-river-jdbc/1.4.0.10/elasticsearch-river-jdbc-1.4.0.10.zip
 
 # Install MySQL JDBC connector
 curl -o mysql-connector-java-5.1.33.zip -L 'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.33.zip/from/http://cdn.mysql.com/'
+
 unzip mysql-connector-java-5.1.33.zip
+
 cp mysql-connector-java-5.1.33/mysql-connector-java-5.1.33-bin.jar plugins/jdbc/
+
 chmod 664 plugins/jdbc/*
 
-# Restart ES
+# Restart Elasticsearch
 /etc/init.d/elasticsearch restart
+
+# Create makers index in Elasticsearch
+curl -XPUT 'localhost:9200/_river/my_jdbc_river_makers/_meta' -d '{
+    "type" : "jdbc",
+    "jdbc" : {
+        "url" : "jdbc:mysql://localhost:3306/makerbase_web",
+        "user" : "makerbase",
+        "password" : "nice2bnice",
+        "sql" : "select * from makers",
+        "index" : "maker_index",
+        "type" : "maker_type"
+    }
+}'
+
+
+# Create products index in Elasticsearch
+curl -XPUT 'localhost:9200/_river/my_jdbc_river_products/_meta' -d '{
+    "type" : "jdbc",
+    "jdbc" : {
+        "url" : "jdbc:mysql://localhost:3306/makerbase_web",
+        "user" : "makerbase",
+        "password" : "nice2bnice",
+        "sql" : "select * from products",
+        "index" : "product_index",
+        "type" : "product_type"
+    }
+}'
 
