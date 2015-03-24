3514d1f7f664e585472e1eb65c075b065b731c91
Gina Trapani <ginatrapani@gmail.com>
2015-03-24 17:26:42 -0400

    Cache user page for one minute

--- webapp/libs/controller/class.UserController.php
+++ webapp/libs/controller/class.UserController.php
@@ -5,18 +5,22 @@ class UserController extends MakerbaseController {
     public function control() {
         parent::control();
         $this->setViewTemplate('user.tpl');
-        $user_dao = new UserMySQLDAO();
-        $user = $user_dao->get($_GET['uid']);
-        $this->addToView('user', $user);
 
-        // Get actions
-        $action_dao = new ActionMySQLDAO();
-        $actions = $action_dao->getUserActivities($user->id);
-        $this->addToView('actions', $actions);
+        //Only cache the activity stream for a minute
+        $this->view_mgr->cache_lifetime = 60;
+        if ($this->shouldRefreshCache() ) {
+            $user_dao = new UserMySQLDAO();
+            $user = $user_dao->get($_GET['uid']);
+            $this->addToView('user', $user);
 
-        $image_proxy_sig = Config::getInstance()->getValue('image_proxy_sig');
-        $this->addToView('image_proxy_sig', $image_proxy_sig);
+            // Get actions
+            $action_dao = new ActionMySQLDAO();
+            $actions = $action_dao->getUserActivities($user->id);
+            $this->addToView('actions', $actions);
 
+            $image_proxy_sig = Config::getInstance()->getValue('image_proxy_sig');
+            $this->addToView('image_proxy_sig', $image_proxy_sig);
+        }
         return $this->generateView();
     }
 }
