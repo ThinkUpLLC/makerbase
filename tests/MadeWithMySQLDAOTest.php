9f50f5ff5b9cabf76f383d6c621956c96f5db35f
Gina Trapani <ginatrapani@gmail.com>
2015-07-14 14:46:40 -0400

    [DB MIGRATION REQ'D] New uses/made_withs table, object, dao

---
+++ tests/MadeWithMySQLDAOTest.php
@@ -0,0 +1,78 @@
+<?php
+require_once dirname(__FILE__).'/init.tests.php';
+
+class MadeWithMySQLDAOTest extends MakerbaseUnitTestCase {
+
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testConstructor() {
+        $madewith_dao = new MadeWithMySQLDAO();
+        $this->assertNotNull($madewith_dao);
+        $this->assertInstanceOf('MadeWithMySQLDAO', $madewith_dao);
+    }
+
+    public function testInsert() {
+        $madewith_dao = new MadeWithMySQLDAO();
+        $madewith = new MadeWith();
+        $madewith->product_id = 1;
+        $madewith->used_product_id = 10;
+
+        $result = $madewith_dao->insert($madewith);
+        $this->assertInstanceOf('MadeWith', $result);
+        $this->assertEquals($result->id, 1);
+    }
+
+    public function testArchiveGetUnarchive() {
+        $madewith_dao = new MadeWithMySQLDAO();
+        $madewith = new MadeWith();
+        $madewith->product_id = 1;
+        $madewith->used_product_id = 10;
+
+        $inserted_madewith = $madewith_dao->insert($madewith);
+        $this->assertFalse($inserted_madewith->is_archived);
+
+        //Archive
+        $madewith_dao->archive($inserted_madewith->uid);
+        $archived_madewith = $madewith_dao->get($inserted_madewith->uid);
+        $this->assertTrue($archived_madewith->is_archived);
+
+        //Unarchive
+        $madewith_dao->unarchive($inserted_madewith->uid);
+        $unarchived_madewith = $madewith_dao->get($inserted_madewith->uid);
+        $this->assertFalse($unarchived_madewith->is_archived);
+    }
+
+    public function testGetByProduct() {
+        $builders = array();
+        $builders[] = FixtureBuilder::build('products', array('id'=>1, 'uid'=>'asdf', 'slug'=>'user'));
+        $builders[] = FixtureBuilder::build('products', array('id'=>2, 'uid'=>'bsdf', 'slug'=>'usee'));
+        $builders[] = FixtureBuilder::build('products', array('id'=>3, 'uid'=>'csdf', 'slug'=>'usee'));
+        $builders[] = FixtureBuilder::build('products', array('id'=>4, 'uid'=>'dsdf', 'slug'=>'usee'));
+        $builders[] = FixtureBuilder::build('products', array('id'=>5, 'uid'=>'esdf', 'slug'=>'usee'));
+        $builders[] = FixtureBuilder::build('products', array('id'=>6, 'uid'=>'fsdf', 'slug'=>'usee'));
+        $builders[] = FixtureBuilder::build('made_withs', array('id'=>1, 'uid'=>'asdf', 'product_id'=>1,
+            'used_product_id'=>2));
+        $builders[] = FixtureBuilder::build('made_withs', array('id'=>2, 'uid'=>'bsdf', 'product_id'=>1,
+            'used_product_id'=>3));
+        $builders[] = FixtureBuilder::build('made_withs', array('id'=>3, 'uid'=>'csdf', 'product_id'=>1,
+            'used_product_id'=>4));
+
+        $product_dao = new ProductMySQLDAO();
+        $product = $product_dao->get('asdf');
+
+        $madewith_dao = new MadeWithMySQLDAO();
+        $madewiths_by_product = $madewith_dao->getByProduct($product);
+        $this->assertEquals(count($madewiths_by_product), 3); //Return 3 max for now for v1 sponsor launch
+        $this->assertInstanceOf('Product', $madewiths_by_product[0]->product);
+        $this->assertInstanceOf('Product', $madewiths_by_product[0]->used_product);
+
+        $this->assertEquals($madewiths_by_product[0]->product->slug, 'user');
+        $this->assertEquals($madewiths_by_product[0]->used_product->slug, 'usee');
+    }
+}
---
+++ webapp/libs/dao/class.MadeWithMySQLDAO.php
@@ -0,0 +1,100 @@
+<?php
+class MadeWithMySQLDAO extends MakerbasePDODAO {
+
+    public function archive($uid) {
+        return $this->setIsArchived($uid, true);
+    }
+
+    public function unarchive($uid) {
+        return $this->setIsArchived($uid, false);
+    }
+
+    private function setIsArchived($uid, $is_archived) {
+        $q = <<<EOD
+UPDATE made_withs SET is_archived = :is_archived WHERE uid = :uid
+EOD;
+        $vars = array (
+            ':uid' => $uid,
+            ':is_archived' => $is_archived
+        );
+        if ($this->profiler_enabled) { Profiler::setDAOMethod(__METHOD__); }
+        //echo self::mergeSQLVars($q, $vars);
+        $ps = $this->execute($q, $vars);
+        return ($this->getUpdateCount($ps) > 0);
+    }
+
+    public function insert(MadeWith $madewith) {
+        $madewith->uid = self::generateRandomString(6);
+        $q = <<<EOD
+INSERT INTO made_withs (
+uid, product_id, used_product_id
+) VALUES (
+:uid, :product_id, :used_product_id
+)
+EOD;
+        $vars = array (
+            ':uid' => $madewith->uid,
+            ':product_id' => $madewith->product_id,
+            ':used_product_id' => $madewith->used_product_id
+        );
+        if ($this->profiler_enabled) { Profiler::setDAOMethod(__METHOD__); }
+        //echo self::mergeSQLVars($q, $vars);
+        $try_insert = true;
+        while ($try_insert) {
+            try {
+                $ps = $this->execute($q, $vars);
+                $try_insert = false;
+                $madewith->id = $this->getInsertId($ps);
+                return $madewith;
+            } catch (PDOException $e) {
+                $message = $e->getMessage();
+                if (strpos($message,'Duplicate entry') !== false && strpos($message,'uid') !== false) {
+                    $try_insert = true;
+                } else {
+                    throw new PDOException($message);
+                }
+            }
+        }
+    }
+
+    public function get($uid) {
+        $q = "SELECT * FROM made_withs mw WHERE uid = :uid";
+        $vars = array ( ':uid' => $uid);
+        if ($this->profiler_enabled) { Profiler::setDAOMethod(__METHOD__); }
+        //echo self::mergeSQLVars($q, $vars);
+        $ps = $this->execute($q, $vars);
+        $madewith = $this->getDataRowAsObject($ps, 'MadeWith');
+        $madewith->is_archived = PDODAO::convertDBToBool($madewith->is_archived);
+        if (!isset($madewith)) {
+            throw new MadeWithDoesNotExistException('MadeWith '.$uid.' does not exist.');
+        }
+        return $madewith;
+    }
+
+    public function getByProduct($product) {
+        $q = <<<EOD
+SELECT mw.*, mw.id AS mw_id, mw.uid AS mw_uid, p.*, p.id AS product_id, p.uid AS product_uid FROM made_withs mw
+INNER JOIN products p ON mw.used_product_id = p.id WHERE mw.is_archived = 0 AND p.is_archived = 0
+AND mw.product_id = :product_id LIMIT 3
+EOD;
+        $vars = array ( ':product_id' => $product->id);
+        if ($this->profiler_enabled) { Profiler::setDAOMethod(__METHOD__); }
+        //echo self::mergeSQLVars($q, $vars);
+        $ps = $this->execute($q, $vars);
+        $rows = $this->getDataRowsAsArrays($ps);
+        $madewiths = array();
+        foreach ($rows as $row) {
+            $madewith = new MadeWith($row);
+            $madewith->id = $row['mw_id'];
+            $madewith->uid = $row['mw_uid'];
+            $madewith->is_archived = false;
+            $used_product = new Product($row);
+            $used_product->id = $row['product_id'];
+            $used_product->uid = $row['product_uid'];
+            $madewith->used_product = $used_product;
+            $madewith->product = $product;
+            $madewiths[] = $madewith;
+        }
+        return $madewiths;
+    }
+}
