fd0fdd3fef473be9b0df5d90d21447cb4b3a0398
Gina Trapani <ginatrapani@gmail.com>
2015-08-13 12:20:01 -0400

    Clean up batch script

--- sql/2015-08-11_user-first-time-bits.sql
+++ sql/2015-08-11_user-first-time-bits.sql
@@ -2,9 +2,9 @@
 -- Add new fields to detect first timers, and temporarily set the default value to null
 --
 ALTER TABLE users
-ADD has_added_maker INT(1) NOT NULL DEFAULT NULL COMMENT 'Whether user has added a maker.' ,
-ADD has_added_product INT(1) NOT NULL DEFAULT NULL COMMENT 'Whether user has added a product.' ,
-ADD has_added_role INT(1) NOT NULL DEFAULT NULL COMMENT 'Whether user has added a role.' ;
+ADD has_added_maker INT(1) DEFAULT NULL COMMENT 'Whether user has added a maker.' ,
+ADD has_added_product INT(1) DEFAULT NULL COMMENT 'Whether user has added a product.' ,
+ADD has_added_role INT(1) DEFAULT NULL COMMENT 'Whether user has added a role.' ;
 
 --
 -- Run batch script here to populate these fields based on whether or not they are null
@@ -18,11 +18,3 @@ CHANGE has_added_maker has_added_maker int(1) NOT NULL DEFAULT '0' COMMENT 'Whet
 CHANGE has_added_product has_added_product int(1) NOT NULL DEFAULT '0' COMMENT 'Whether user has added a product.' AFTER has_added_maker,
 CHANGE has_added_role has_added_role int(1) NOT NULL DEFAULT '0' COMMENT 'Whether user has added a role.' AFTER has_added_product;
 
---
--- Fun usage stats!
---
-select count(*) as added_product_but_no_maker from users where has_added_product = 1 and has_added_maker = 0;
-
-select count(*) as added_maker_but_no_product from users where has_added_product = 0 and has_added_maker = 1;
-
-select count(*) as added_maker_product_but_no_role from users where has_added_product = 1 and has_added_maker = 1 and has_added_role = 0;
