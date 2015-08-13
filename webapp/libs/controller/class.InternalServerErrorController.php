5afad00838bb4a17e5a021ce7c4c3d56b7c8f4d2
Gina Trapani <ginatrapani@gmail.com>
2015-08-13 17:28:05 -0400

    error_log 404s and 500s

--- webapp/libs/controller/class.InternalServerErrorController.php
+++ webapp/libs/controller/class.InternalServerErrorController.php
@@ -4,6 +4,7 @@ class InternalServerErrorController extends MakerbaseController {
 
     public function control() {
         parent::control();
+        error_log('HTTP 500 '.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
         $this->setViewTemplate('500.tpl');
         if (isset($_SERVER["SERVER_PROTOCOL"])) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
--- webapp/libs/controller/class.PageNotFoundController.php
+++ webapp/libs/controller/class.PageNotFoundController.php
@@ -4,7 +4,11 @@ class PageNotFoundController extends MakerbaseController {
 
     public function control() {
         parent::control();
+        error_log('HTTP 404 '.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
         $this->setViewTemplate('404.tpl');
+        if (isset($_SERVER["SERVER_PROTOCOL"])) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found', true, 404);
+        }
         return $this->generateView();
     }
 }
