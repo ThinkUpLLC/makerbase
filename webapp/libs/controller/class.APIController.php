2a513bf49e5b6efeeeacbaa19cdc73e408c5461a
Gina Trapani <ginatrapani@gmail.com>
2016-02-03 09:26:24 -0500

    API: Remove nested JSON data

--- webapp/libs/controller/class.APIController.php
+++ webapp/libs/controller/class.APIController.php
@@ -11,8 +11,9 @@ class APIController extends MakerbaseController {
             if (isset($_GET['endpoint']) && $_GET['endpoint'] == 'signups') {
                 $start = (isset($_GET['start']))?date('Y-m-d', strtotime($_GET['start'])):date('Y-m-d');
                 $end = (isset($_GET['end']))?date('Y-m-d', strtotime($_GET['end'])):date('Y-m-d');
-                $json_data = $user_dao->getTotalSignups($start, $end);
-                $json_data[0]["value"] = intval($json_data[0]["value"]);
+                $total_signups = $user_dao->getTotalSignups($start, $end);
+                $total_signups[0]["value"] = intval($total_signups[0]["value"]);
+                $json_data = $total_signups[0];
             } else {
                 $json_data = array('value'=>-1);
             }
